c181d67446e703a524f7aeab6eb441731a953032
Kyrene <Kyrene.Aflores@gmail.com>
2024-02-07 12:07:38 -0600

    finished audio Player

--- react-vite/src/components/Navigation/AudioPlayer/AudioPlayer.jsx
+++ react-vite/src/components/Navigation/AudioPlayer/AudioPlayer.jsx
@@ -1,36 +1,24 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-import { useDispatch, useSelector } from 'react-redux'
-import { thunkGetAllSongs } from '../../../redux/song'
-import { useEffect } from 'react'
-import './AudioPlayer.css'
+import React from "react";
+import ReactPlayer from "react-player";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import "./AudioPlayer.css";
 
-
-function AudioPlayer(){
-
-    const dispatch = useDispatch()
-    const allSongs = useSelector(state => state.song)
-
-    // console.log("ALLSONGS", allSongs)
-    useEffect(()=>{
-        dispatch(thunkGetAllSongs)
-    },[dispatch])
-
-
-
-
-    if(!allSongs[21])return null
-    return (
-        <div className='audio-player-main-cont'>
-            <h3>Hello</h3>
-            <ReactPlayer
-                // url={allSongs[21].audio_file}
-                controls={true}
-                playing={true}
-                width='100%'
-                height='50px'
-            />
-        </div>
-    );
+function AudioPlayer() {
+  const dispatch = useDispatch();
+  const currAudio = useSelector((state) => state.audio);
+  const isPlaying = useSelector((state) => state.audio.isPlaying);
+  if (!currAudio[1]) return null;
+  return (
+    <div className="audio-player-main-cont">
+      <ReactPlayer
+        url={currAudio[1]}
+        controls={true}
+        playing={isPlaying.isPlaying}
+        width="100%"
+        height="50px"
+      />
+    </div>
+  );
 }
-export default AudioPlayer
+export default AudioPlayer;
